52da2d09e2dab15ab26bebc6c0ae99469ecb311a
Norton Pengra <npengra317@gmail.com>
2017-04-24 18:53:59 -0700

    Add comment blocks

--- CS133Assignment1/DateTime.h
+++ CS133Assignment1/DateTime.h
@@ -2,37 +2,79 @@
 //	File: DateTime.h
 //
 //	class: DateTime
-//-----------------------------------------------------------------------------
+//    File:             DateTime.cpp
+//
+//    Title:            DateTime Class
+//
+//    Description:	This file contains the class definition for DateTime
+//
+//    Programmer:	Paul Bladek & Norton Pengra
+//   
+//    Date:             4/24/2017
+// 
+//    Version:          1.0
+//  
+//    Environment:   Hardware: IBM Pentium  
+//                   Software:  WinXP or later or .NET for execution;
+//
+//    Compiles under Microsoft C++ 2005
+// 
+//	  class DateTime:
+//
+//	  Properties:
+//		Date m_Date;  
+//		CTime m_CTime;	
+//
+//	  Virtual Non-inline:
+//		virtual bool operator==(const Comparable &other)const;
+//		virtual bool operator<(const Comparable &other)const;
+//		virtual void input(istream& sin);
+//		virtual void print(ostream& sout)const;
+//
+//	  Virtual Inline:
+//		virtual bool operator!=(const Comparable &other)const
+//		virtual bool operator>(const Comparable &other)const
+//		virtual bool operator<=(const Comparable &other)const
+//		virtual bool operator>=(const Comparable &other)const
+//
+//    History Log:
+//                      4/7/08  PB  completed version 1.0
+// ----------------------------------------------------------------------------
 #ifndef DATETIME_H
 #define DATETIME_H
 #include "Date.h"
 #include "CTime.h"
 
-class DateTime : Date, CTime {
-public:
+using namespace std;
+namespace NP_DATETIME {
+	class DateTime : Date, CTime {
+	public:
+		// Constructors
+		DateTime();
+		DateTime(short dayOfMonth, short month = 0, short year = 0, short hour = 0, short minute = 0, short second = 0);
+		DateTime(const Date & date, const CTime & time);
 
-	DateTime();
-	DateTime(short dayOfMonth, short month = 0, short year = 0, short hour = 0, short minute = 0, short second = 0);
-	DateTime(const Date & date, const CTime & time);
+		// pure virtuals that MUST be defined
+		virtual bool operator==(const Comparable &other)const;
+		virtual bool operator!=(const Comparable &other)const
+			{ return !((*this) == other); }
+		virtual bool operator<(const Comparable &other)const;
+		virtual bool operator>(const Comparable &other)const
+			{ return !((*this) < other) && !((*this) == other); }
+		virtual bool operator<=(const Comparable &other)const
+			{ return !((*this) > other); }
+		virtual bool operator>=(const Comparable &other)const
+			{ return !((*this) < other); }
+		
+		virtual void input(istream& sin);
+		virtual void print(ostream& sout)const;
 
-	// pure virtuals that MUST be defined
-	virtual bool operator==(const Comparable &other)const;
-	virtual bool operator!=(const Comparable &other)const
-		{return !((*this) == other);}
-	virtual bool operator<(const Comparable &other)const;
-	virtual bool operator>(const Comparable &other)const
-		{return !((*this) < other) && !((*this) == other);}
-	virtual bool operator<=(const Comparable &other)const
-		{return !((*this) > other);}
-	virtual bool operator>=(const Comparable &other)const
-		{return !((*this) < other);}
-	virtual void input(istream& sin);
-	virtual void print(ostream& sout)const;
+	private:
+		Date m_date;
+		CTime m_time;
+	};
 
-private:
-	Date m_date;
-	CTime m_time;
-
-};
-
+	ostream& operator<<(ostream& sout, const DateTime& date);
+	istream& operator>>(istream& sin, DateTime& date);
+}
 #endif
