4a60c4a5a9d649ac8c57b85aeda05f6827e5afb2
Norton Pengra <npengra317@gmail.com>
2017-04-25 16:49:14 -0700

    add simple constructor and comparison tests

--- CSS133Assignment1Tests/DateTimeTests.cpp
+++ CSS133Assignment1Tests/DateTimeTests.cpp
@@ -14,11 +14,67 @@ namespace CSS133Assignment1Tests
 
 		// Constructor tests
 
-		TEST_METHOD(EmptyConstructorTest)
+		TEST_METHOD(emptyConstructor) {
+			DateTime date1;
+			Assert::AreEqual(currentDate(), date1.getDayOfMonth());
+			Assert::AreEqual(currentMonth(), date1.getMonth());
+			Assert::AreEqual(currentYear(), date1.getYear());
+			validateTime(date1);
+		}
+
+		// Comparison tests
+
+		TEST_METHOD(MinimumComparisonTest)
 		{
 			DateTime date1(0, 0, LOWYEAR, 0, 0, 0);
 			DateTime date2(0, 0, LOWYEAR, 0, 0, 0);
 			Assert::IsTrue(date1 == date2);
+		}
+
+	private:
+		void validateTime(CTime time) {
+			Assert::IsTrue(time.getHour() >= 0 && time.getHour() < 24);
+			Assert::IsTrue(time.getMinute() >= 0 && time.getMinute() < 60);
+			Assert::IsTrue(time.getSecond() >= 0 && time.getSecond() < 60);
+		}
+
+		short currentDate() {
+			time_t rawtime;
+			tm rightnow;
+			time(&rawtime);
+			rightnow = *localtime(&rawtime);
+
+			return rightnow.tm_mday;
+		}
+
+		short currentMonth() {
+			time_t rawtime;
+			tm rightnow;
+			time(&rawtime);
+			rightnow = *localtime(&rawtime);
+
+			return rightnow.tm_mon;
+		}
+
+		short currentYear() {
+			time_t rawtime;
+			tm rightnow;
+			time(&rawtime);
+			rightnow = *localtime(&rawtime);
+
+			return rightnow.tm_year + 1900;
+		}
+
+		bool isToday(Date date) {
+			time_t rawtime;
+			tm rightnow;
+			time(&rawtime);
+			rightnow = *localtime(&rawtime);
+
+			bool yearMatch = ((short)(rightnow.tm_year + 1900) == date.getYear());
+			bool monthMatch = ((short)(rightnow.tm_mon) == date.getMonth());
+			bool dayMatch = ((short)(rightnow.tm_mday) == date.getDayOfMonth());
+			return yearMatch && monthMatch && dayMatch;
 		}
 	};
 }
