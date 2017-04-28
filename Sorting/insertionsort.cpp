cb47805328aaae1c5268d3e25511068cc6dc5398
Norton Pengra <npengra317@gmail.com>
2017-04-28 14:13:56 -0700

    move to np_isort namespace

--- Sorting/insertionsort.cpp
+++ Sorting/insertionsort.cpp
@@ -3,68 +3,69 @@
 #include <string>
 #include <vector>
 using namespace std;
+namespace NP_ISORT {
+	class A {
+	public:
+		A(int num) {};
+		virtual int method() = 0;
+		virtual bool operator>(A & lhs) = 0;
+		virtual bool operator<(A & lhs) = 0;
+		virtual bool operator==(A & lhs) = 0;
+		virtual bool operator>=(A & lhs) = 0;
+		virtual bool operator<=(A & lhs) = 0;
+		virtual bool operator!=(A & lhs) = 0;
+		virtual void print() = 0;
+	};
 
-class A {
-public:
-	A(int num) {};
-	virtual int method() = 0;
-	virtual bool operator>(A & lhs) = 0;
-	virtual bool operator<(A & lhs) = 0;
-	virtual bool operator==(A & lhs) = 0;
-	virtual bool operator>=(A & lhs) = 0;
-	virtual bool operator<=(A & lhs) = 0;
-	virtual bool operator!=(A & lhs) = 0;
-	virtual void print() = 0;
-};
+	class B : virtual public A {
+	public:
+		B(int num) : A(num) { this->number = num; }
+		virtual int method() { return this->number; }
+		virtual bool operator>(A & lhs) { return this->number < dynamic_cast<B&>(lhs).method(); };
+		virtual bool operator<(A & lhs) { return this->number > dynamic_cast<B&>(lhs).method(); };
+		virtual bool operator==(A & lhs) { return this->number == dynamic_cast<B&>(lhs).method(); };
+		virtual bool operator>=(A & lhs) { return this->number >= dynamic_cast<B&>(lhs).method(); };
+		virtual bool operator<=(A & lhs) { return this->number <= dynamic_cast<B&>(lhs).method(); };
+		virtual bool operator!=(A & lhs) { return this->number != dynamic_cast<B&>(lhs).method(); };
+		virtual void print() { cout << this->number << ' '; };
 
-class B : virtual public A {
-public:
-	B(int num) : A(num) { this->number = num; }
-	virtual int method() { return this->number; }
-	virtual bool operator>(A & lhs) { return this->number < dynamic_cast<B&>(lhs).method(); };
-	virtual bool operator<(A & lhs) { return this->number > dynamic_cast<B&>(lhs).method(); };
-	virtual bool operator==(A & lhs) { return this->number == dynamic_cast<B&>(lhs).method(); };
-	virtual bool operator>=(A & lhs) { return this->number >= dynamic_cast<B&>(lhs).method(); };
-	virtual bool operator<=(A & lhs) { return this->number <= dynamic_cast<B&>(lhs).method(); };
-	virtual bool operator!=(A & lhs) { return this->number != dynamic_cast<B&>(lhs).method(); };
-	virtual void print() { cout << this->number << ' '; };
+	private:
+		int number;
+	};
 
-private:
-	int number;
-};
+	//Sorting in non decreasing order
+	void printArray(A ** array, int size) {
+		for (int index = 0; index < size; index++)
+			array[index]->print();
 
-//Sorting in non decreasing order
-void printArray(A ** array, int size) {
-	for (int index = 0; index < size; index++)
-		array[index]->print();
-
-	cout << "\n";
-}
-void insertionsort(A ** array, int fromIndex, int toIndex) {
-	if (fromIndex < 1)
-		fromIndex = 1;
-
-	for (int outerIndex = fromIndex; outerIndex < toIndex; outerIndex++) {
-		int innerIndex = outerIndex - 1;
-		A * temp = array[outerIndex];
-
-		while (innerIndex >= 0 && *temp < *array[innerIndex]) {
-			array[innerIndex + 1] = array[innerIndex];
-			innerIndex--;
-		}
-
-		array[innerIndex + 1] = temp;
-		cout << "After pass " << outerIndex << "  : ";
-		//Printing array after pass
-		printArray(array, toIndex);
+		cout << "\n";
 	}
-}
-int main() {
-	int size = 9;
-	A ** array = nullptr;
-	array = new A*[size] { &B(1), &B(7), &B(3), &B(0), &B(3), &B(1), &B(9), &B(9), &B(7) };
-	cout << "Initial Array : ";
-	printArray(array, size);
-	insertionsort(array, 0, size);
-	return 0;
+	void insertionsort(A ** array, int fromIndex, int toIndex) {
+		if (fromIndex < 1)
+			fromIndex = 1;
+
+		for (int outerIndex = fromIndex; outerIndex < toIndex; outerIndex++) {
+			int innerIndex = outerIndex - 1;
+			A * temp = array[outerIndex];
+
+			while (innerIndex >= 0 && *temp < *array[innerIndex]) {
+				array[innerIndex + 1] = array[innerIndex];
+				innerIndex--;
+			}
+
+			array[innerIndex + 1] = temp;
+			cout << "After pass " << outerIndex << "  : ";
+			//Printing array after pass
+			printArray(array, toIndex);
+		}
+	}
+	int main() {
+		int size = 9;
+		A ** array = nullptr;
+		array = new A*[size] { &B(1), &B(7), &B(3), &B(0), &B(3), &B(1), &B(9), &B(9), &B(7) };
+		cout << "Initial Array : ";
+		printArray(array, size);
+		insertionsort(array, 0, size);
+		return 0;
+	}
 }
