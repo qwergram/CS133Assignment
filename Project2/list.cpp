03c56844eae8bf0e5115756ab734b1e39b0f2c68
Qwergram <npengra317@gmail.com>
2017-05-02 09:57:04 -0700

    rename to proper namespace and classname

--- Project2/list.cpp
+++ Project2/list.cpp
@@ -5,103 +5,15 @@
 //   Functions: main
 //-----------------------------------------------------------------------------
 
-//-----------------------------------------------------------------------------
-//   Title: list Class
-//   Description: This file contains the class definition for list, with a test driver
-//
-//   Programmer: 
-//    Object Oriented Programming Using C++, Edition 2 By Ira Pohl
-//      modified by Paul Bladek
-// 
-//   Date: original: Summer 1996
-//   Version: 1.06
-//
-//   Environment: Intel Xeon PC
-//     Software: MS Windows 7 for execution;
-//     Compiles under Microsoft Visual C++.Net 2010
-//
-//   class list:
-//
-//     Properties:
-//       listelem *head -- front end of list
-//       listelem *tail -- back end of list
-//       unsigned m_size -- number of elements in the list
-//
-//     Methods:
-// 
-//       inline: 
-//         list() -- construct the empty list
-//         list(size_t n_elements, datatype datum) -- create a list of a specific size, all elements the same
-//         ~list() { release(); } -- destructor
-//         unsigned getSize()const  -- accessor, returns m_size
-//         iterator begin()const -- returns address of first element
-//         iterator end()const -- returns address of last element
-//         datatype& front()const  -- returns first element data
-//         datatype& back()const -- returns last element data
-//         bool empty()const -- true if no elements
-//
-//       non-inline:
-//         list(const list& x); -- copy constructor
-//         list(iterator b, iterator e) -- constructor using iterators
-//         void push_front(datatype datum) -- insert element at front of list
-//         datatype pop_front(); -- removes front element and returns the data from that element
-//         void push_back(datatype datum) -- insert element at back of list 
-//         datatype pop_back() -- removes back element and returns the data from that element
-//         void release() -- removes all items from list
-//         list operator=(const list & rlist) -- returns a copy of rlist
-//         datatype& operator[](int index) -- for l-value
-//         const datatype& operator[](int index)const -- for r-value
-//
-//    Public Nested Structures:
-//
-//      struct listelem -- list cell
-//
-//        properties:
-//          datatype data -- the actual datum
-//          listelem *next -- forward link
-//          listelem *prev -- backward link
-//        constructor:
-//          listelem(datatype c, listelem* p, listelem* n, listelem* p) -- struct constructor
-//
-//     class iterator
-//        Public Methods
-// 
-//           inline: 
-//            listelem* operator->() -- same as for pointer
-//            datatype& operator*() -- same as for pointer
-//            operator listelem*() -- conversion
-//            iterator(listelem* p = nullptr) -- constructor
-// 
-//           non-inline: 
-//            iterator operator++() -- pre-increment
-//            iterator operator--() -- pre-decrement
-//            iterator operator++(int) -- post-increment
-//            iterator operator--(int) -- post-decrement
-// 
-//        Private Properties
-//            listelem* ptr -- current listelem or NULL
-//
-//   History Log:
-//     summer, 1996 original  IP completed version 1.0 
-//     May 11, 2000,  PB  completed version 1.01 
-//     March 8, 2002, PB  completed version 1.02 
-//     April 10, 2003, PB  completed version 1.03 
-//     April 30, 2003, PB  completed version 1.04 
-//     May 10, 2005, PB  completed version 1.05 
-//     April 15, 2008, PB  completed version 1.06
-//     April 20, 2010, PB completed version 1.07 
-//     April 15, 2011, PB completed version 1.09 
-//-----------------------------------------------------------------------------
 
 #include "list.h"
 
-namespace PB_ADT
+namespace NP_ADT
 {
-
 	//-----------------------------------------------------------------------------
 	// constructor
 	//-----------------------------------------------------------------------------
-	list::list(size_t n_elements, datatype datum)
+	DLCL::DLCL(size_t n_elements, datatype datum)
 		:m_size(0), head(nullptr), tail(nullptr)
 	{
 		if (n_elements <= 0)
@@ -113,10 +25,10 @@ namespace PB_ADT
 	//-----------------------------------------------------------------------------
 	// copy constructor
 	//-----------------------------------------------------------------------------
-	list::list(const list& x)
+	DLCL::DLCL(const DLCL& x)
 		: m_size(0), head(nullptr), tail(nullptr)
 	{
-		list::iterator r_it = x.begin();
+		DLCL::iterator r_it = x.begin();
 		while (r_it != nullptr)
 			push_front(*r_it++);
 	}
@@ -124,7 +36,7 @@ namespace PB_ADT
 	//-----------------------------------------------------------------------------
 	// constructor using iterators, copies from b to one before e
 	//-----------------------------------------------------------------------------
-	list::list(iterator b, iterator e)
+	DLCL::DLCL(iterator b, iterator e)
 		:m_size(0), head(nullptr), tail(nullptr)
 	{
 		while (b != e)
@@ -134,7 +46,7 @@ namespace PB_ADT
 	//-----------------------------------------------------------------------------
 	// empties the list
 	//-----------------------------------------------------------------------------
-	void list::release()
+	void DLCL::release()
 	{
 		while (head != nullptr)
 			pop_front();
@@ -143,9 +55,9 @@ namespace PB_ADT
 	//-----------------------------------------------------------------------------
 	// prints out a list
 	//-----------------------------------------------------------------------------
-	ostream& operator<<(ostream& sout, const list& x)
+	ostream& operator<<(ostream& sout, const DLCL& x)
 	{
-		list::iterator p = x.begin(); // gets x.h
+		DLCL::iterator p = x.begin(); // gets x.h
 		sout << "(";
 		while (p != nullptr)
 		{
@@ -161,7 +73,7 @@ namespace PB_ADT
 	//-----------------------------------------------------------------------------
 	// insert element at front of list
 	//-----------------------------------------------------------------------------
-	void list::push_front(datatype datum)
+	void DLCL::push_front(datatype datum)
 	{
 		listelem* temp = new listelem(datum, nullptr, head);
 		m_size++;
@@ -178,11 +90,11 @@ namespace PB_ADT
 
 	// returns a copy of rlist
 	//-----------------------------------------------------------------------------
-	list list::operator=(const list & rlist)
+	DLCL DLCL::operator=(const DLCL & rlist)
 	{
 		if (&rlist != this)
 		{
-			list::iterator r_it = rlist.begin();
+			DLCL::iterator r_it = rlist.begin();
 			release();
 			while (r_it != 0)
 				push_front(*r_it++);
@@ -193,7 +105,7 @@ namespace PB_ADT
 	//-----------------------------------------------------------------------------
 	// pre-increment
 	//-----------------------------------------------------------------------------
-	list::iterator list::iterator::operator++()
+	DLCL::iterator DLCL::iterator::operator++()
 	{
 		if (ptr == nullptr)
 			throw runtime_error("nullptr pointer");
@@ -204,7 +116,7 @@ namespace PB_ADT
 	//-----------------------------------------------------------------------------
 	// post-increment
 	//-----------------------------------------------------------------------------
-	list::iterator list::iterator::operator++(int)
+	DLCL::iterator DLCL::iterator::operator++(int)
 	{
 		if (ptr == nullptr)
 			throw runtime_error("nullptr pointer");
@@ -216,7 +128,7 @@ namespace PB_ADT
 	//-----------------------------------------------------------------------------
 	// pre-decrement
 	//-----------------------------------------------------------------------------
-	list::iterator list::iterator::operator--()
+	DLCL::iterator DLCL::iterator::operator--()
 	{
 		// REPLACE THE DUMMY CODE BELOW WITH YOUR OWN
 		iterator temp = nullptr;
@@ -227,7 +139,7 @@ namespace PB_ADT
 	//-----------------------------------------------------------------------------
 	// post-decrement
 	//-----------------------------------------------------------------------------
-	list::iterator list::iterator::operator--(int)
+	DLCL::iterator DLCL::iterator::operator--(int)
 	{
 		// REPLACE THE DUMMY CODE BELOW WITH YOUR OWN
 		iterator temp = nullptr;
@@ -237,7 +149,7 @@ namespace PB_ADT
 	//-----------------------------------------------------------------------------
 	// removes front element and returns the data from that element
 	//-----------------------------------------------------------------------------
-	datatype list::pop_front()
+	datatype DLCL::pop_front()
 	{
 		if (head == nullptr)
 			throw runtime_error("Empty list");
@@ -257,7 +169,7 @@ namespace PB_ADT
 	//-----------------------------------------------------------------------------
 	// [] operator -- l-value
 	//-----------------------------------------------------------------------------
-	datatype& list::operator[](int index)
+	datatype& DLCL::operator[](int index)
 	{
 		iterator it;
 		if (index >= 0)
@@ -281,7 +193,7 @@ namespace PB_ADT
 	//-----------------------------------------------------------------------------
 	// [] operator -- r-value
 	//-----------------------------------------------------------------------------
-	const datatype& list::operator[](int index)const
+	const datatype& DLCL::operator[](int index)const
 	{
 		iterator it;
 		if (index >= 0)
@@ -310,13 +222,13 @@ namespace PB_ADT
   //-----------------------------------------------------------------------------
 int main(void)
 {
-	using PB_ADT::list; // not the same as std::list<class T>
+	using NP_ADT::DLCL; // not the same as std::list<class T>
 	try
 	{
-		list mylist(3, 'X');
-		list mylist2;
-		list mylist3(mylist);
-		list mylist4(mylist.begin(), mylist.end());
+		DLCL mylist(3, 'X');
+		DLCL mylist2;
+		DLCL mylist3(mylist);
+		DLCL mylist4(mylist.begin(), mylist.end());
 		cout << "mylist: " << mylist << endl;
 		cout << "mylist2: " << mylist2 << endl;
 		cout << "mylist3: " << mylist3 << endl;
@@ -327,7 +239,7 @@ int main(void)
 		mylist2 = mylist; // a copy
 		cout << "mylist: " << mylist << endl;
 		// create a new iterator pointing to the beginning of mylist
-		list::iterator listit(mylist.begin());
+		DLCL::iterator listit(mylist.begin());
 		cout << listit++->data << " ";
 		cout << listit->data << endl;
 		cout << "mylist: ";
--- Project2/list.h
+++ Project2/list.h
@@ -4,22 +4,111 @@
 #include <cstdlib>
 using namespace std;
 
-namespace PB_ADT {
+//-----------------------------------------------------------------------------
+//   Title: list Class
+//   Description: This file contains the class definition for list, with a test driver
+//
+//   Programmer: 
+//    Object Oriented Programming Using C++, Edition 2 By Ira Pohl
+//      modified by Paul Bladek
+// 
+//   Date: original: Summer 1996
+//   Version: 1.06
+//
+//   Environment: Intel Xeon PC
+//     Software: MS Windows 7 for execution;
+//     Compiles under Microsoft Visual C++.Net 2010
+//
+//   class list:
+//
+//     Properties:
+//       listelem *head -- front end of list
+//       listelem *tail -- back end of list
+//       unsigned m_size -- number of elements in the list
+//
+//     Methods:
+// 
+//       inline: 
+//         list() -- construct the empty list
+//         list(size_t n_elements, datatype datum) -- create a list of a specific size, all elements the same
+//         ~list() { release(); } -- destructor
+//         unsigned getSize()const  -- accessor, returns m_size
+//         iterator begin()const -- returns address of first element
+//         iterator end()const -- returns address of last element
+//         datatype& front()const  -- returns first element data
+//         datatype& back()const -- returns last element data
+//         bool empty()const -- true if no elements
+//
+//       non-inline:
+//         list(const list& x); -- copy constructor
+//         list(iterator b, iterator e) -- constructor using iterators
+//         void push_front(datatype datum) -- insert element at front of list
+//         datatype pop_front(); -- removes front element and returns the data from that element
+//         void push_back(datatype datum) -- insert element at back of list 
+//         datatype pop_back() -- removes back element and returns the data from that element
+//         void release() -- removes all items from list
+//         list operator=(const list & rlist) -- returns a copy of rlist
+//         datatype& operator[](int index) -- for l-value
+//         const datatype& operator[](int index)const -- for r-value
+//
+//    Public Nested Structures:
+//
+//      struct listelem -- list cell
+//
+//        properties:
+//          datatype data -- the actual datum
+//          listelem *next -- forward link
+//          listelem *prev -- backward link
+//        constructor:
+//          listelem(datatype c, listelem* p, listelem* n, listelem* p) -- struct constructor
+//
+//     class iterator
+//        Public Methods
+// 
+//           inline: 
+//            listelem* operator->() -- same as for pointer
+//            datatype& operator*() -- same as for pointer
+//            operator listelem*() -- conversion
+//            iterator(listelem* p = nullptr) -- constructor
+// 
+//           non-inline: 
+//            iterator operator++() -- pre-increment
+//            iterator operator--() -- pre-decrement
+//            iterator operator++(int) -- post-increment
+//            iterator operator--(int) -- post-decrement
+// 
+//        Private Properties
+//            listelem* ptr -- current listelem or NULL
+//
+//   History Log:
+//     summer, 1996 original  IP completed version 1.0 
+//     May 11, 2000,  PB  completed version 1.01 
+//     March 8, 2002, PB  completed version 1.02 
+//     April 10, 2003, PB  completed version 1.03 
+//     April 30, 2003, PB  completed version 1.04 
+//     May 10, 2005, PB  completed version 1.05 
+//     April 15, 2008, PB  completed version 1.06
+//     April 20, 2010, PB completed version 1.07 
+//     April 15, 2011, PB completed version 1.09 
+//-----------------------------------------------------------------------------
+
+
+namespace NP_ADT {
 
 	typedef char datatype;
 
-	class list
+	class DLCL
 	{
 	public:
 		struct listelem; // forward declarations
 		class iterator;
 		// constructors
-		list() : head(nullptr), tail(nullptr), m_size(0) {}
-		list(size_t n_elements, datatype datum);
-		list(const list& x);
-		list(iterator b, iterator e);
+		DLCL() : head(nullptr), tail(nullptr), m_size(0) {}
+		DLCL(size_t n_elements, datatype datum);
+		DLCL(const DLCL& x);
+		DLCL(iterator b, iterator e);
 
-		~list() { release(); }
+		~DLCL() { release(); }
 		unsigned getSize() const { return m_size; }
 		iterator begin() const { return head; }
 		iterator end() const { return tail; }
@@ -31,7 +120,7 @@ namespace PB_ADT {
 		datatype& back() const { return tail->data; }
 		bool empty()const { return head == nullptr || tail == nullptr; }
 		void release();
-		list operator=(const list & rlist);
+		DLCL operator=(const DLCL & rlist);
 		datatype& operator[](int index);
 		const datatype& operator[](int index) const;
 	private:
@@ -63,6 +152,6 @@ namespace PB_ADT {
 		};
 	};
 
-	ostream& operator<<(ostream& sout, const list& x);
+	ostream& operator<<(ostream& sout, const DLCL& x);
 
 }
