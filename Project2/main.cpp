0f9f9e93cd1060953f4f196be612f126168c56a4
Norton Pengra <npengra317@gmail.com>
2017-05-04 17:24:35 -0700

    add main.cpp

---
+++ Project2/main.cpp
@@ -0,0 +1,49 @@
+#include "list.h"
+#include <iostream>
+#include <cassert>
+#include <exception>
+#include <cstdlib>
+using namespace std;
+
+//---------------------------------------------------------------------------
+// driver to test list
+//---------------------------------------------------------------------------
+int main(void)
+{
+	using NP_ADT::CDLL; // not the same as std::list<class T>
+	try
+	{
+		/*CDLL<char> mylist(3, 'X');
+		CDLL<char> mylist2;
+		CDLL<char> mylist3(mylist);
+		CDLL<char> mylist4(mylist.begin(), mylist.end());
+		cout << "mylist: " << mylist << endl;
+		cout << "mylist2: " << mylist2 << endl;
+		cout << "mylist3: " << mylist3 << endl;
+		cout << "mylist4: " << mylist4 << endl;
+		mylist.push_front('Y');
+		mylist.push_front('Z');
+		cout << mylist.pop_front() << endl;
+		mylist2 = mylist; // a copy
+		cout << "mylist: " << mylist << endl;
+		// create a new iterator pointing to the beginning of mylist
+		CDLL<char>::iterator listit(mylist.begin());
+		cout << listit++->data << " ";
+		cout << listit->data << endl;
+		cout << "mylist: ";
+		for (int i = 0; i < static_cast<int>(mylist.getSize()); i++)
+		cout << mylist[i] << " ";
+		cout << "mylist: " << endl;
+		mylist.release();
+		cout << "mylist: " << mylist << endl;
+		cout << "mylist2: " << mylist2 << endl;*/
+	}
+	catch (exception e)
+	{
+		cerr << "program terminated: " << e.what() << endl;
+		cin.get();
+		return EXIT_FAILURE;
+	}
+	cin.get();  // keep window open
+	return EXIT_SUCCESS;
+}
