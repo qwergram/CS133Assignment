2959f53d5495daf9bb540fde3a109b27922a0bbb
Qwergram <npengra317@gmail.com>
2017-05-10 10:04:28 -0700

    add 80 char check script

---
+++ 80CharCheck.ps1
@@ -0,0 +1,22 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$folder
+);
+
+$code = Get-ChildItem $folder -Recurse | Where-Object { $_.extension -eq ".h" -or $_.extension -eq ".cpp" -or $_.extension -eq ".hpp" }
+
+
+For ($i = 0; $i -lt $code.Length; $i++) {
+    
+    # Write-Output $code[$i].FullName;
+    $reader = [System.IO.File]::OpenText($code[$i].FullName);
+    For ($k = 1;;$k++) {
+        $line = $reader.ReadLine();
+        if ($line -eq $null) { break }
+
+        if ($line.length -ge 80) {
+            Write-Host -NoNewline $code[$i].FullName "Line (" $k ") @ " $line.Length "|" $line "`n"
+        }
+    }
+
+}
