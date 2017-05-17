5b1f8a4729f76dc7142fa8915e07bcb8a1840e0a
Norton Pengra <npengra317@gmail.com>
2017-05-16 17:25:59 -0700

    rearrange inheritance rules Read a bit on how inheritance works in C++. Fixed things based upon those readings

--- Project2/queue.cpp
+++ Project2/queue.cpp
@@ -1,5 +1,24 @@
 #include "queue.h"
 
 namespace NP_ADT {
+	template<class datatype>
+	inline unsigned Queue<datatype>::getSize() const
+	{
+		return CDLL<datatype>::getSize();
+	}
+
+	template<class datatype>
+	void Queue<datatype>::push(datatype & element)
+	{
+		push_back(element);
+	}
+
+	template<class datatype>
+	datatype & Queue<datatype>::pop()
+	{
+		datatype popped_value = CDLL<datatype>::pop_front();
+		datatype & popped_value_reference = popped_value;
+		return popped_value_reference;
+	}
 
 }
--- Project2/queue.h
+++ Project2/queue.h
@@ -6,37 +6,37 @@ namespace NP_ADT {
 	template <class datatype>
 	class Queue : virtual protected CDLL<datatype> {
 	public:
+
 		Queue(void) 
-			: handle(nullptr), m_size(0),
+			: // handle(nullptr), m_size(0),
 			CDLL<datatype>::CDLL() {}
 		Queue(size_t n_elements, datatype datum) 
-			: handle(nullptr), m_size(0), 
+			: // handle(nullptr), m_size(0), 
 			CDLL<datatype>::CDLL(n_elements, datum) { };
 		Queue(const CDLL & queue) 
-			: handle(nullptr), m_size(0), 
+			: // handle(nullptr), m_size(0), 
 			CDLL<datatype>::CDLL(queue) { };
 		Queue(iterator begin, iterator end) 
-			: handle(nullptr), m_size(0), 
+			: // handle(nullptr), m_size(0), 
 			CDLL<datatype>::CDLL(begin, end) { };
 		
-		~Queue() { release(); };
+		virtual ~Queue() { release(); };
 
-		unsigned getSize() const { return m_size; }
-		bool empty() const { return CDLL<datatype>::empty(); }
+		virtual unsigned getSize() const;
+		virtual bool empty() const { return (handle == nullptr); };
 		
 		void release() { while (handle != nullptr) { pop(); } }
 
 		iterator begin() const { return head(); }
 		iterator end() const { return tail(); }
 		
-		void push(datatype & element) { CDLL<datatype>::push_back(element); m_size++; }
-		// void push(datatype element) { CDLL<datatype>::push_back(element); m_size++; }
-		datatype & pop() { m_size--; return CDLL<datatype>::pop_front(); }
+		void push(datatype & element);
+		datatype & pop();
 
-		node * head() { return CDLL<datatype>::head(); }
-		node * tail() { return CDLL<datatype>::tail(); }
+		node * head() { return handle; }
+		node * tail() { return (handle == nullptr) ? nullptr : handle->prev; }
 	protected:
-		node * handle;
-		unsigned m_size; // number of elements in the list 
+		// node * handle;
+		// unsigned m_size; // number of elements in the list 
 	};
 }
