aaf1961fe859cfd6168aaf0ad7af8879549cc9d9
Norton Pengra <npengra317@gmail.com>
2017-05-16 19:28:48 -0700

    start queue pointer "tests"

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,5 +1,7 @@
 #include "list.h"
 #include "list.cpp"
+#include "queue.h"
+#include "queue.cpp"
 #include <iostream>
 #include <cassert>
 #include <exception>
@@ -12,9 +14,40 @@ using namespace std;
 int main(void)
 {
 	using NP_ADT::CDLL; // not the same as std::list<class T>
+	using NP_ADT::Queue;
 	try
 	{
 		CDLL<char> mylist(3, 'X');
+		CDLL<char> mylist2;
+		CDLL<char> mylist3(mylist);
+		CDLL<char> mylist4(mylist.begin(), mylist.end());
+		cout << "mylist: " << mylist << endl;
+		cout << "mylist2: " << mylist2 << endl;
+		cout << "mylist3: " << mylist3 << endl;
+		cout << "mylist4: " << mylist4 << endl;
+		mylist.push_front('Y');
+		mylist.push_front('Z');
+		cout << mylist.pop_front() << endl;
+		mylist2 = mylist; // a copy
+		cout << "mylist: " << mylist << endl;
+		// create a new iterator pointing to the beginning of mylist
+		CDLL<char>::iterator listit(mylist.begin());
+		cout << listit++->data << " ";
+		cout << listit->data << endl;
+		cout << "mylist: ";
+		for (unsigned int i = 0; i < mylist.getSize(); i++)
+			cout << mylist[i] << " ";
+		cout << endl;
+		cout << "mylist[1] = 'N';" << endl;
+		mylist[1] = 'N';
+		cout << "mylist: " << mylist << endl;
+		mylist.release();
+		cout << "mylist: " << mylist << endl;
+		cout << "mylist2: " << mylist2 << endl;
+
+		// Queue Pointer Tests
+		
+		myqueue = &Queue<char>(3, 'X');
 		CDLL<char> mylist2;
 		CDLL<char> mylist3(mylist);
 		CDLL<char> mylist4(mylist.begin(), mylist.end());
