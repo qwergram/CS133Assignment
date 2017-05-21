510f94fcd30ada59d16bdd0f834f13b57fb0ac80
Norton Pengra <npengra317@gmail.com>
2017-05-20 21:47:39 -0700

    header block for exception class

--- Project2/npadtexcept.h
+++ Project2/npadtexcept.h
@@ -1,3 +1,32 @@
+//-----------------------------------------------------------------------------
+//   Title: methodnotsupported exception
+//   Description: This file contains the class 
+//				  declarations for MethodNotSupported
+//
+//   Programmer: 
+//    Norton Pengra
+// 
+//   Date: original: Spring 2017
+//   Version: 1.10
+//
+//   Environment: Intel Xeon PC
+//     Software: MS Windows 10 for execution;
+//     Compiles under Microsoft Visual C++.Net 2017
+//
+//   class publicqueue : (inherits from cdll)
+//
+//     Methods:
+// 
+//       inline: 
+//         const char* what(); -- pass through for runtime_error's what()
+//		   All other methods are overridden and will throw an exception
+//		   if called.
+//
+//
+//   History Log:
+//	   May 18, 2017, NP completed version 1.10
+//-----------------------------------------------------------------------------
+
 #pragma once
 #include <exception>
 #include <stdexcept>
@@ -7,6 +36,8 @@ namespace NP_ADT {
 	class MethodNotSupported : public runtime_error {
 	public:
 		MethodNotSupported() : runtime_error("method not supported") {}
+		
+		// Not strictly neccessary
 		virtual const char* what() const throw () {
 			return runtime_error::what();
 		}
