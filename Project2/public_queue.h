3bccedef6647e5abcf1d830570418f9cb1605bd0
Norton Pengra <npengra317@gmail.com>
2017-05-20 21:42:32 -0700

    move definitions to inline

--- Project2/public_queue.h
+++ Project2/public_queue.h
@@ -1,4 +1,42 @@
-#pragma once
+//-----------------------------------------------------------------------------
+//   Title: circular queue
+//   Description: This file contains the class 
+//				  definition for queue
+//
+//   Programmer: 
+//    Norton Pengra
+// 
+//   Date: original: Spring 2017
+//   Version: 1.10
+//
+//   Environment: Intel Xeon PC
+//     Software: MS Windows 10 for execution;
+//     Compiles under Microsoft Visual C++.Net 2017
+//
+//   class publicqueue : (inherits from cdll)
+//
+//     Methods:
+// 
+//       inline: 
+//         queue() -- construct the empty list
+//         queue(size_t n_elements, datatype datum) -- create a 
+//					list of a specific size, all elements the same
+//         ~queue() -- destructor
+//         unsigned getSize()const  -- accessor, returns m_size
+//         iterator begin()const -- returns address of first element
+//         iterator end()const -- returns address of last element
+//         bool empty()const -- true if no elements
+//         void push(datatype datum) -- insert element at back of queue 
+//         datatype pop(); -- removes front element and returns the data 
+//									from that element
+//		   All other methods are overridden and will throw an exception
+//		   if called.
+//
+//
+//   History Log:
+//	   May 18, 2017, NP completed version 1.10
+//-----------------------------------------------------------------------------
+
 #pragma once
 #include "list.h"
 #include "npadtexcept.h"
@@ -19,8 +57,8 @@ namespace NP_ADT {
 
 		virtual ~PublicQueue() { release(); };
 
-		void push(datatype & element);
-		datatype pop();
+		void push(datatype & element) { CDLL<datatype>::push_back(element); }
+		datatype pop() { return CDLL<datatype>::pop_front(); }
 
 		// Overload
 		virtual void push_front(datatype datum) { throw MethodNotSupported(); }
@@ -28,17 +66,9 @@ namespace NP_ADT {
 
 		virtual datatype pop_front() { throw MethodNotSupported(); }
 		virtual datatype pop_back() { throw MethodNotSupported(); }
+
+		datatype & operator[](int index) { throw MethodNotSupported(); };
+		const datatype & operator[](int index) const { throw MethodNotSupported(); };
 	};
 
-	template<class datatype>
-	inline void PublicQueue<datatype>::push(datatype & element)
-	{
-		CDLL<datatype>::push_back(element);
-	}
-
-	template<class datatype>
-	inline datatype PublicQueue<datatype>::pop()
-	{
-		return CDLL<datatype>::pop_front();
-	}
 }
