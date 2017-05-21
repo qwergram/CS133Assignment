78e59b101eb0d96962bfcdcea9205b0f810ec8d4
Norton Pengra <npengra317@gmail.com>
2017-05-20 21:42:14 -0700

    add what overload

--- Project2/npadtexcept.h
+++ Project2/npadtexcept.h
@@ -7,5 +7,8 @@ namespace NP_ADT {
 	class MethodNotSupported : public runtime_error {
 	public:
 		MethodNotSupported() : runtime_error("method not supported") {}
+		virtual const char* what() const throw () {
+			return runtime_error::what();
+		}
 	};
 }
