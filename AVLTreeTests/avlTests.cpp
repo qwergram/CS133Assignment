5001b82db2b9db8462a1a0ed0a695150e77f8f07
Norton Pengra <npengra317@gmail.com>
2017-05-24 19:51:28 -0700

    sample tests with vectors

--- AVLTreeTests/avlTests.cpp
+++ AVLTreeTests/avlTests.cpp
@@ -1,2 +1,17 @@
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "..\AVLTree\avl.h"
+
+TEST_CASE("A wonderful test", "[vector]") {
+	std::vector<int> v(5);
+
+	REQUIRE(v.size() == 5);
+	REQUIRE(v.capacity() >= 5);
+
+	SECTION("resizing bigger changes size and capacity") {
+		v.resize(10);
+		
+		REQUIRE(v.size() == 10);
+		REQUIRE(v.capacity() < 10);
+	}
+}
