d3e7d1592828fc1fdd59feb8f148d8b449b36a9a
Norton Pengra <npengra317@gmail.com>
2017-05-24 19:43:52 -0700

    Copy rotation definitions from old project https://github.com/SeleniumK/data_structures2/blob/master/src/bst.py#L196

--- AVLTree/avl.h
+++ AVLTree/avl.h
@@ -1,1 +1,84 @@
-#pragma once
+#ifndef AVL_H
+#define AVL_H
+#include "bst.h"
+using namespace NP_BST;
+
+namespace NP_AVL {
+	template <class T>
+	class avl : public bst<T> {
+	public:
+		using bst::bst;
+	protected:
+		node<T> * rotateRight(node <T> * target);
+		node<T> * rotateLeft(node <T> * target);
+		node<T> * rotateRightLeft(node <T> * target);
+		node<T> * rotateLeftRight(node <T> * target);
+		node<T> * rebalance(node<T> * target);
+		int getHeightDifference(const node<T> * const target) const;
+	};
+	template<class T>
+	inline node<T> * avl<T>::rotateRight(node<T>* target)
+	{
+		node<T> * temp = target->right;
+		target->right = temp->left;
+		temp->left = target;
+		return * temp;
+	}
+	template<class T>
+	inline node<T>* avl<T>::rotateLeft(node<T>* target)
+	{
+		node<T> * temp;
+		temp = target->left;
+		target->left = temp->right;
+		temp->right = target;
+		return * temp;
+	}
+	template<class T>
+	inline node<T>* avl<T>::rotateRightLeft(node<T>* target)
+	{
+		node<T> * temp;
+		temp = target->right;
+		target->right = rotateLeft(*temp);
+		return rotateRight(*target);
+	}
+	template<class T>
+	inline node<T>* avl<T>::rotateLeftRight(node<T>* target)
+	{
+		node<T> * temp;
+		temp = target->left;
+		target->left = rotateRight(*temp);
+		return rotateLeft(*target);
+	}
+	template<class T>
+	inline node<T>* avl<T>::rebalance(node<T>* target)
+	{
+		int bal_factor = getHeightDifference(*target);
+		if (bal_factor > 1)
+		{
+			if (getHeightDifference(*target->left) > 0)
+				*target = rotateLeft(*target);
+			else
+				*target = rotateLeftRight(*target);
+		}
+		else if (bal_factor < -1)
+		{
+			if (getHeightDifference(*target->right) > 0)
+				*target = rotateRigthLeft(*target);
+			else
+				*target = rotateRight(*target);
+		}
+		return * target;
+	}
+	template<class T>
+	inline int avl<T>::getHeightDifference(const node<T> * const target) const
+	{
+		int left = 0;
+		int right = 0;
+		if (target->left != nullptr)
+			left = target->left->getHeight();
+		if (traget->right != nullptr)
+			right = target->right->getHeight();
+		return left - right;
+	}
+}
+#endif
