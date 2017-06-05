ebfa25a77bd3b54fed61fd990c340a67833fc3c1
Norton Pengra <npengra317@gmail.com>
2017-06-05 16:02:07 -0700

    copy comment header block templates

--- AVLTree/set.h
+++ AVLTree/set.h
@@ -1,5 +1,60 @@
 #ifndef SET_H
 #define SET_H
+//-----------------------------------------------------------------------------
+// Title: 
+// Description: 
+// Meta: 
+// Programmer: 
+//		Written by Paul Bladek
+//		Modified by Norton Pengra
+// Date: June 2017
+// Version: 1.10
+// Environment: 
+//      Intel i7
+//		Software: MS Windows 10 for execution;
+//		Compiles under Microsoft Visual C++.Net 2017
+// Classes:
+//		class Node<T>:
+//			A node containing a value, with two pointers
+//			to a left and right child. Also holds a height
+//			attribute.
+//		
+//		class Bst<T>:
+//	!NEW!	Public Sub Classes:
+//	!NEW!		BFIterator
+//	!NEW!			Constructor(tree) -- creates a BF traversal iterator of given tree
+//	!NEW!			next() -- returns the next node in BF traversal
+//	!NEW!			endOfTree() -- returns True if BFT has reached the end
+//	!NEW!			getLast() -- retrieve the last thing returned by next
+//			Public Methods:
+//				Constructors
+//				getroot() -- used for retrieving root node
+//				operator = overload -- used for reassigning trees
+//				operator += overload -- used for adding trees with syntactic sugars
+//					(can be used to add another T or another Bst<T>)
+//				operator + overload -- used for adding trees with syntactic sugars
+//				isempty() -- returns true of the tree contains no items
+//	!NEW!		contains(T item) -- returns true if item is in tree
+//				insert(T item) -- inserts item into tree
+//				delTree() -- deletes entire tree
+//				print() -- "pretty" prints the tree up to 7 layers
+//				print ... () -- related methods for print
+//				popNode(Node<T> *& t) -- pop the node t from the tree
+//				popLow(Node<T> *& t) -- pop the leftmost child of node t
+//				popHigh(Node<T> *& t) -- pop the rightmost child of node t
+//				getHeight() -- get height of the tree
+//				setHeight() -- recursively set the height attribute of each node.
+//				setLevel -- related method for print
+//				getNumberOfNodes -- get number of nodes in tree
+//				destructor -- calls delTree
+//			Protected Attributes:
+//				Node<T> * root -- the root of the tree
+//				Node<T> ** parentptr -- holding pointer needed by some functions
+//				
+// History Log:
+//		revised June, 2017
+//--------------------------------------------------------------------
+
 
 #include "avl.h"
 using namespace std;
@@ -30,8 +85,17 @@ namespace NP_SET
 		Node<T>* &getroot() { return this->root; }
 	};
 
-	
-
+	//--------------------------------------------------------------------
+	// Title: 
+	// Description: 
+	// Called By: 
+	// Calls: 
+	// Parameters: 
+	// Returns: 
+	// Throws:
+	// History Log: https://github.com/qwergram/CS133Assignment/blame/project3/AVLTree/
+	// Test Plan: https://travis-ci.org/qwergram/CS133Assignment/
+	//--------------------------------------------------------------------
 	template<class T>
 	inline void Set<T>::print(Node<T>* cur, ostream & out) const
 	{
@@ -43,6 +107,17 @@ namespace NP_SET
 		}
 	}
 
+	//--------------------------------------------------------------------
+	// Title: 
+	// Description: 
+	// Called By: 
+	// Calls: 
+	// Parameters: 
+	// Returns: 
+	// Throws:
+	// History Log: https://github.com/qwergram/CS133Assignment/blame/project3/AVLTree/
+	// Test Plan: https://travis-ci.org/qwergram/CS133Assignment/
+	//--------------------------------------------------------------------
 	template<class T>
 	inline void Set<T>::printXlevel(Node<T>* cur, ostream & out) const
 	{
@@ -79,6 +154,17 @@ namespace NP_SET
 		}
 	}
 
+	//--------------------------------------------------------------------
+	// Title: 
+	// Description: 
+	// Called By: 
+	// Calls: 
+	// Parameters: 
+	// Returns: 
+	// Throws:
+	// History Log: https://github.com/qwergram/CS133Assignment/blame/project3/AVLTree/
+	// Test Plan: https://travis-ci.org/qwergram/CS133Assignment/
+	//--------------------------------------------------------------------
 	template<class T>
 	inline bool Set<T>::remove(T target)
 	{
@@ -91,6 +177,17 @@ namespace NP_SET
 		return true;
 	}
 
+	//--------------------------------------------------------------------
+	// Title: 
+	// Description: 
+	// Called By: 
+	// Calls: 
+	// Parameters: 
+	// Returns: 
+	// Throws:
+	// History Log: https://github.com/qwergram/CS133Assignment/blame/project3/AVLTree/
+	// Test Plan: https://travis-ci.org/qwergram/CS133Assignment/
+	//--------------------------------------------------------------------
 	template<class T>
 	inline Set<T> Set<T>::Union(Set<T> rhs)
 	{
@@ -99,6 +196,17 @@ namespace NP_SET
 		return newTree;
 	}
 
+	//--------------------------------------------------------------------
+	// Title: 
+	// Description: 
+	// Called By: 
+	// Calls: 
+	// Parameters: 
+	// Returns: 
+	// Throws:
+	// History Log: https://github.com/qwergram/CS133Assignment/blame/project3/AVLTree/
+	// Test Plan: https://travis-ci.org/qwergram/CS133Assignment/
+	//--------------------------------------------------------------------
 	template<class T>
 	inline Set<T> Set<T>::intersection(Set<T> rhs)
 	{
@@ -109,6 +217,7 @@ namespace NP_SET
 		// https://travis-ci.org/qwergram/CS133Assignment/builds/239752012
 		// Also here:
 		// https://github.com/qwergram/CS133Assignment/commit/8602ca33fc8357960ca269d0aa0aaf31b6bd5925
+		// Please learn from this.
 		// Much love,
 		// Past Norton.
 		auto lhsIterator = typename Set<T>::BFIterator(*this);
