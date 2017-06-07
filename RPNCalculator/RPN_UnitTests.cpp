829fc9a02b50b4decbc891cea536528cd0ffc655
Tabitha Roemish <tabitharoemish@gmail.com>
2017-06-06 18:45:53 -0700

    make testing file

---
+++ RPNCalculator/RPN_UnitTests.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN 
+#include "catch.hpp"
+#include "RPNCalc.h"
+
+using namespace PB_CALC;
+
+TEST_CASE("Operation Methods")
+{
+	SECTION("Method: add Test")
+	{
+		CRPNCalc test;
+
+		m_stack.push_front(3);
+	}
+}
