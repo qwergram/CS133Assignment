20cf8cdc948b7b7d261564814cff3c6f0ef61113
Tabitha Roemish <tabitharoemish@gmail.com>
2017-06-07 20:09:09 -0700

    unit tests for + - * / %

--- RPNCalculator/RPN_UnitTests.cpp
+++ RPNCalculator/RPN_UnitTests.cpp
@@ -7,11 +7,80 @@ using namespace PB_CALC;
 
 TEST_CASE("Operation Methods")
 {
-	SECTION("Method: add Test")
-	{
-		RPNTestHelper test;
+	RPNTestHelper test;
 
-		REQUIRE(test.expectedInputOutput("3 5 +", "8"));
+	SECTION("Method: add()")
+	{
 		
+		REQUIRE(test.expectedInputOutput("3 5 +", "8"));
+		REQUIRE(test.expectedInputOutput("2 4 3 +", "7"));
+		REQUIRE(test.expectedInputOutput("c", ""));
+		REQUIRE(test.expectedInputOutput("3 4 + 6", "6"));
+		REQUIRE(test.expectedInputOutput("4 +", "10"));
+		REQUIRE(test.expectedInputOutput("-12 +", "-2"));
+		REQUIRE(test.expectedInputOutput("5 +", "3"));
+		REQUIRE(test.expectedInputOutput("3 +", "10"));
+	}
+
+	SECTION("Method: subtract()")
+	{
+		REQUIRE(test.expectedInputOutput("5 6 -", "-1"));
+		REQUIRE(test.expectedInputOutput("3 9 4 -", "5"));
+		REQUIRE(test.expectedInputOutput("-", "-2"));
+		REQUIRE(test.expectedInputOutput("c", ""));
+		REQUIRE(test.expectedInputOutput("-1 5 -", "-6"));
+	}
+
+	SECTION("Method: multiply()")
+	{
+		REQUIRE(test.expectedInputOutput("3 9 7 2 4 *", "8"));
+		REQUIRE(test.expectedInputOutput("*", "56"));
+		REQUIRE(test.expectedInputOutput("*", "504"));
+		REQUIRE(test.expectedInputOutput("*", "1512"));
+		REQUIRE(test.expectedInputOutput("*", "<<error>>"));
+		REQUIRE(test.expectedInputOutput("c", ""));
+		REQUIRE(test.expectedInputOutput("-4 -6 *", "24"));
+		REQUIRE(test.expectedInputOutput("-2 *", "48"));
+	}
+
+	SECTION("Method: divide()")
+	{
+		REQUIRE(test.expectedInputOutput("16 4 2 /", "2"));
+		REQUIRE(test.expectedInputOutput("/", "8"));
+		REQUIRE(test.expectedInputOutput("c", ""));
+		REQUIRE(test.expectedInputOutput("2 5 /", "0.4"));
+		REQUIRE(test.expectedInputOutput("9 3 7 /", "0.428571"));
+		REQUIRE(test.expectedInputOutput("-1 /", "-0.428571"));
+		REQUIRE(test.expectedInputOutput("-1 / ", "0.428571"));
+	}
+
+	SECTION("Method: mod()") 
+	{
+		REQUIRE(test.expectedInputOutput("16 8 6 %", "2"));
+		REQUIRE(test.expectedInputOutput("%", "0"));
+		REQUIRE(test.expectedInputOutput("c", ""));
+
+		REQUIRE(test.expectedInputOutput("", ""));
+		REQUIRE(test.expectedInputOutput("", ""));
+		REQUIRE(test.expectedInputOutput("", ""));
+		
+	}
+
+	SECTION("Method: exp()")
+	{
+		REQUIRE(test.expectedInputOutput("", ""));
+		REQUIRE(test.expectedInputOutput("", ""));
+		REQUIRE(test.expectedInputOutput("", ""));
+		REQUIRE(test.expectedInputOutput("", ""));
+		REQUIRE(test.expectedInputOutput("", ""));
+	}
+
+	SECTION("Method: neg()")
+	{
+		REQUIRE(test.expectedInputOutput("", ""));
+		REQUIRE(test.expectedInputOutput("", ""));
+		REQUIRE(test.expectedInputOutput("", ""));
+		REQUIRE(test.expectedInputOutput("", ""));
+		REQUIRE(test.expectedInputOutput("", ""));
 	}
 }
