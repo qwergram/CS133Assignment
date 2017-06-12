f71caacee3cd42b41b3af2df97a209847c4694bc
Norton Pengra <npengra317@gmail.com>
2017-06-11 18:33:27 -0700

    move definitions to header file

--- RPNCalculator/RPNTestHelper.h
+++ RPNCalculator/RPNTestHelper.h
@@ -73,4 +73,50 @@ namespace P4_RPNCALC
 		CRPNCalc calc;
 	};
 }
+
+//--------------------------------------------------------------------
+// Title:		getStackOutput
+// Description: returns the string representation of the first
+//				item on the stack. Operation occurs after the
+//				input operation.
+// Called By:	testing units
+// Calls:		calc dependency injection
+// Parameters:	input command
+// Returns:		string representation of last output from command
+//				from the stack.
+// History Log: https://github.com/qwergram/CS133Assignment/blame/Tabitha/RPNCalculator/RPNTestHelper.cpp
+// Test Plan: https://travis-ci.org/qwergram/CS133Assignment/
+//--------------------------------------------------------------------
+inline string P4_RPNCALC::RPNTestHelper::getStackOutput(string input) {
+	ostringstream ss;
+	calc.depInj_setBuffer(input);
+	while (!calc.getBuffer().empty())
+		calc.depInj_runParse();
+	ss << calc.getOutput();
+	return ss.str();
+}
+
+//--------------------------------------------------------------------
+// Title:		getOutput
+// Description: returns the string representation of the last
+//				operation. Operation occurs after the
+//				input operation.
+// Called By:	testing units
+// Calls:		calc dependency injection
+// Parameters:	input command
+// Returns:		string representation of last output from command
+//				not from the stack.
+// History Log: https://github.com/qwergram/CS133Assignment/blame/Tabitha/RPNCalculator/RPNTestHelper.cpp
+// Test Plan: https://travis-ci.org/qwergram/CS133Assignment/
+//--------------------------------------------------------------------
+inline string P4_RPNCALC::RPNTestHelper::getOutput(string input) {
+	ostringstream ss;
+	calc.depInj_setBuffer(input);
+	while (!calc.getBuffer().empty())
+		calc.depInj_runParse();
+	ss << calc.depInj_lastOutput();
+	return ss.str();
+}
+
+
 #endif
