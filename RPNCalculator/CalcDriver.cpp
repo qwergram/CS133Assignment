2db7c2a051d68332d60ae542f655bef4ad1e64e7
Norton Pengra <npengra317@gmail.com>
2017-06-12 19:02:00 -0700

    remove testing

--- RPNCalculator/CalcDriver.cpp
+++ RPNCalculator/CalcDriver.cpp
@@ -37,7 +37,7 @@ using namespace std;
 //		6/11/2017 - https://github.com/qwergram/CS133Assignment/blame/Tabitha/RPNCalculator/RPNCalcStart.cpp
 //----------------------------------------------------------------------------
 
-int main2(void) {
+int main(void) {
 	using P4_RPNCALC::CRPNCalc;
 	CRPNCalc myCalc;
 
